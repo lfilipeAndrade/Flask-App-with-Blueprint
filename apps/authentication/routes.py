2daab1a34b07ac7c4151d6922380f3c887f8933c
Luis Filipe Andrade <lfilipepython@gmail.com>
2023-02-04 20:53:16 -0300

    2nd commit

--- apps/authentication/routes.py
+++ apps/authentication/routes.py
@@ -5,3 +5,7 @@ from apps.authentication import blueprint
 @blueprint.route('/login')
 def login():
     return render_template( 'accounts/login.html')
+
+@blueprint.route('/register')
+def register():
+    return render_template( 'accounts/register.html')
