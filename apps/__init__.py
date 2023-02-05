c481391653d844631e7583c34f5c1e149ba3c51c
Luis Filipe Andrade <lfilipepython@gmail.com>
2023-02-04 21:07:07 -0300

    3nd commit

--- apps/__init__.py
+++ apps/__init__.py
@@ -3,15 +3,19 @@ from .config import db
 from importlib import import_module
 
 def register_blueprints(app):
-    for module_name in ('authentication', 'home'):
+
+    blueprints_list = ['authentication', 'home']
+
+    for module_name in blueprints_list:
         module = import_module('apps.{}.routes'.format(module_name))
         app.register_blueprint(module.blueprint)
 
 def create_app(config):
     app = Flask(__name__)
-    
+
+    # Building a connection with your mysql database using SQLalchemy
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://{}:{}@127.0.0.1:3306/{}'\
-        .format('client','7362','custumer_review')
+        .format('database_user','database_pwd','database_name')
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     register_blueprints(app)
